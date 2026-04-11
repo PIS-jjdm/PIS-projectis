14993285a9114346f5e3863c5bcd94f2b9155187
Matúš Moravčík <xmorav48@stud.fit.vut.cz>
2026-04-11 22:56:13 +0200

    feat: add project crud operations

--- services/project-service/src/main/java/org/pis/project/repositories/ProjectRepository.java
+++ services/project-service/src/main/java/org/pis/project/repositories/ProjectRepository.java
@@ -2,8 +2,10 @@ package org.pis.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 import org.pis.project.entities.ProjectEntity;
 
-public interface ProjectRepository extends JpaRepository<ProjectEntity, Integer> {
-
+public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+    List<ProjectEntity> findBySubjectId(String subjectId);
 }
--- services/project-service/src/main/java/org/pis/project/services/ProjectService.java
+++ services/project-service/src/main/java/org/pis/project/services/ProjectService.java
@@ -3,6 +3,7 @@ package org.pis.project.services;
 import java.util.List;
 
 import org.pis.project.entities.ProjectEntity;
+import org.pis.project.mappers.ProjectMapper;
 import org.pis.project.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,19 +13,32 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public List<ProjectEntity> getAllProjects() {
-        return projectRepository.findAll();
+    @Autowired
+    private ProjectMapper projectMapper;
+
+    public List<ProjectEntity> listProjects(String subjectId) {
+        return projectRepository.findBySubjectId(subjectId);
     }
 
-    public ProjectEntity getProjectById(Integer id) {
-        return projectRepository.findById(id).orElse(null);
+    public ProjectEntity getProject(Long id) {
+        return projectRepository.findById(id)
+                .orElseThrow();
     }
 
     public ProjectEntity createProject(ProjectEntity project) {
+        project.setId(null);
         return projectRepository.save(project);
     }
 
-    public void deleteProject(Integer id) {
-        projectRepository.deleteById(id);
+    public ProjectEntity updateProject(ProjectEntity projectUpdated) {
+        ProjectEntity existingProject = getProject(projectUpdated.getId());
+        projectMapper.updateEntityFromRequest(projectUpdated, existingProject);
+        return projectRepository.save(existingProject);
+    }
+
+    public ProjectEntity deleteProject(Long id) {
+        ProjectEntity project = getProject(id);
+        projectRepository.delete(project);
+        return project;
     }
 }
