29b8031e758123a2549702996f2bf24f3140ec5f
Matúš Moravčík <xmorav48@stud.fit.vut.cz>
2026-04-12 22:10:28 +0200

    feat: add transactional

--- services/project-service/src/main/java/org/pis/project/services/ProjectService.java
+++ services/project-service/src/main/java/org/pis/project/services/ProjectService.java
@@ -7,37 +7,42 @@ import org.pis.project.entities.ProjectEntity;
 import org.pis.project.exceptions.ResourceNotFoundException;
 import org.pis.project.mappers.ProjectMapper;
 import org.pis.project.repositories.ProjectRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
-    @Autowired
-    private ProjectRepository projectRepository;
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
 
-    @Autowired
-    private ProjectMapper projectMapper;
-
+    @Transactional(readOnly = true)
     public List<ProjectEntity> listProjects(String subjectId) {
         return projectRepository.findBySubjectId(subjectId);
     }
 
+    @Transactional(readOnly = true)
     public ProjectEntity getProject(UUID id) {
         return projectRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Project not found with id: " + id));
     }
 
+    @Transactional
     public ProjectEntity createProject(ProjectEntity project) {
         project.setId(null);
         return projectRepository.save(project);
     }
 
+    @Transactional
     public ProjectEntity updateProject(ProjectEntity projectUpdated) {
         ProjectEntity existingProject = getProject(projectUpdated.getId());
         projectMapper.updateEntityFromRequest(projectUpdated, existingProject);
         return projectRepository.save(existingProject);
     }
 
+    @Transactional
     public ProjectEntity deleteProject(UUID project_id) {
         ProjectEntity project = getProject(project_id);
         projectRepository.delete(project);
